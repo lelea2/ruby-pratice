a0b9fac41f75eb14ea9308f36d4fa1e9c4349757
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-03-06 22:53:44 -0800

    Update for test pass

--- graded-assignments/course01/module03/assignment01-Recipe-Hunter/recipefinder/app/models/recipe.rb
+++ graded-assignments/course01/module03/assignment01-Recipe-Hunter/recipefinder/app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ActiveRecord::Base
   include HTTParty
 
-  key_value = ENV['FOOD2FORK_KEY']
+  key_value = ENV['FOOD2FORK_KEY'] || '0c0ede5e194a25e2d87eab6f47cbb467'
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api/search"
   default_params key: key_value
