f9dcb02c44580a0961c7325ddbadeb3249ffee18
lelea2 <daomaikhanh1988@yahoo.com>
2017-01-10 15:28:55 -0800

    Initial check in for rails api

--- rails_5_api/config/routes.rb
+++ rails_5_api/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults:{ format: :json } do
+  # constraints subdomain: 'api' do
+  namespace :api do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update, :destroy]
+      resources :users
     end
   end
+  # end
 end
